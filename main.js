e0ec58d933d8b0dbbfe79c823fca670419d983a9
ashmika22kar173 <80112977+ashmika22kar173@users.noreply.github.com>
2021-07-30 10:12:07 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,41 @@
+song = "";
+song2 = "";
+leftwrist = 0;
+rightwrist = 0;
+
+function preload() {
+    song = loadSound("music.mp3");
+    song2 = loadSound("music2.mp3")
+}
+
+function setup() {
+    canvas = createCanvas(500, 400);
+    canvas.center();
+
+    video = createCapture(VIDEO);
+    video.hide();
+
+    posenet = ml5.poseNet(video, modelloded);
+    posenet.on('pose', gotresult);
+}
+
+function draw() {
+    image(video, 0, 0, 500, 400);
+}
+
+
+function modelloded() {
+    console.log("modelloded");
+
+}
+
+function gotresult(results) {
+    if (results.length > 0) {
+        console.log(results);
+
+        leftwrist = results[0].pose.leftWrist;
+        rightwrist = results[0].pose.rightWrist;
+        console.log(" leftwrist = " + leftwrist + " rigthwrist = " + rightwrist);
+    }
+
+}
